c83adc0105a60c2764243fdefe1d92de87e027bb
Tinku Bansal <tinkubansal21@gmail.com>
2021-04-08 15:29:47 +1000

    when signUp page is opened the main page should be hidden and vice versa

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ var selectEl = document.querySelector("#teamList")
 var submitEl = document.querySelector("#submitButton");
 var inputEl = document.querySelector("#userName");
 var selectEl = document.querySelector("#teamList");
-var localIndex=0; 
-  
+var localIndex=0;
+var signUpPageEl = document.querySelector("#signUpPage");
+var mainPageEl = document.querySelector("#mainPage");
+var settingsEl = document.querySelector("#settings");
+
 var requestUrl = 'https://api.squiggle.com.au/?q=teams';
     
 fetch(requestUrl)
@@ -11,7 +14,8 @@ fetch(requestUrl)
         return response.json();
     })
     .then(function (data) {
-        console.log(data)
+        console.log(data);
+        mainPageEl.style.display = "none";
         for (var i = 0; i < data.teams.length; i++) {
               
             var createOption = document.createElement('option');      
@@ -22,6 +26,7 @@ fetch(requestUrl)
             selectEl.appendChild(createOption);
                
             }
+        
     });
  
 
@@ -34,4 +39,35 @@ function saveData(event){
     userData.team = selectEl.value;
     console.log(userData);
     var local = localStorage.setItem(localIndex, JSON.stringify(userData));
+
+    // display main page and hide first page
+    displayMainPage();
 }
+
+// display main page and hide first page
+function displayMainPage(){
+    // hides signUp page
+    signUpPageEl.dataset.state = "hidden";
+    signUpPageEl.style.display = "none";
+
+    // display main page
+    mainPageEl.dataset.state = "visible";
+    mainPageEl.style.display = "block";
+
+}
+
+// display signUp page and hides main page
+function displaySignUpPage(){
+    // hides signUp page
+    mainPageEl.dataset.state = "hidden";
+    mainPageEl.style.display = "none";
+
+    // display main page
+    signUpPageEl.dataset.state = "visible";
+    signUpPageEl.style.display = "block";
+
+}
+
+
+// event listner for settings button
+settingsEl.addEventListener('click',displaySignUpPage);
